66ad38d9477e9afeeed23eed1e81b5821ccd6a91
dunyung1 <dshege4@gmail.com>
2019-01-27 22:44:15 +0300

    created test credential file

--- credential.py
+++ credential.py
@@ -1,4 +1,7 @@
 class Credential:
     '''
-    Cla
+    Class that creates new instance of credentials
     '''
+    def __init__(self,username,password):
+        self.myusername=username
+        self.mypassword=password
