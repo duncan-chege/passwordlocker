eeb2de978c5999601ad067e5ff960208b3a3bbf5
dunyung1 <dshege4@gmail.com>
2019-01-27 18:42:41 +0300

    create a instance variables for my objects

--- user.py
+++ user.py
@@ -1,1 +1,12 @@
-class
+class User:
+    '''
+    Class that generates new users
+    '''
+    user_list=[]
+
+    def __init__(self,firstname,email):
+        '''
+        defining properties for our object
+        '''
+        self.myfirstname=firstname
+        self.myemail=email
