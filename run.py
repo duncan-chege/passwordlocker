dcf3e5a1607849b3530187f0796ad92672e4ab18
dunyung1 <dshege4@gmail.com>
2019-01-28 01:27:45 +0300

    make some progress tomorrow

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3.6
-from user import User, Credential
+from user import User
+from credential import Credential
 
 def create_user(fname,email):
     '''
@@ -8,12 +9,10 @@ def create_user(fname,email):
     new_user = User(fname,email)
     return new_user
 
-def create_credential(fname,email):
-    '''
-    Function to create a new user
-    '''
-    new_user = User(fname,email)
-    return new_user
+def create_credential(uname,pword):
+
+    new_credential = Credential(uname,pword)
+    return new_credential
 
 def save_users(user):
     '''
@@ -21,11 +20,27 @@ def save_users(user):
     '''
     user.save_user()
 
-def del_users(user):
+def save_credentials(credential):
+
+    credential.save_credential()
+
+def delete_credential(credential):
     '''
-    Function to delete a user
+    Function to delete a credential
     '''
-    user.delete_user()
+    credential.delete_credential()
+
+def find_credential(username):
+    '''
+    Function that finds a credential by username and returns the credential
+    '''
+    return Credential.find_by_username(username)
+
+def check_existing_credentials(username):
+    '''
+    Function that check if a credential exists with that number and return a Boolean
+    '''
+    return Credential.credential_exist(username)
 
 def display_users():
     '''
@@ -33,11 +48,19 @@ def display_users():
     '''
     return User.display_users()
 
+def display_credentials():
+    '''
+    Function that returns all the saved credentials
+    '''
+    return Credentials.display_credentials()
+
 def main():
     print("Welcome to your user list. What is your name?")
-    user_name = input()
+    kwanza_name = input()
+    print("And username?")
+    kwanza_uname = input()
 
-    print(f"Hello {user_name}. what would you like to do?")
+    print(f"Hello {kwanza_uname}. what would you like to do?")
     print('\n')
 
     while True:
@@ -51,13 +74,20 @@ def main():
 
                     print ("First name -")
                     f_name = input()
-
                     print("Email address - ")
                     e_address = input()
 
+                    print ("User name -")
+                    u_name = input()
+                    print("Password - ")
+                    p_word = input()
+
+
                     save_users(create_user(f_name,e_address)) # create and save new user.
+                    save_credentials(create_credential(u_name,p_word)) # create and save new user.
+
                     print ('\n')
-                    print(f"New User {f_name} created")
+                    print(f"New User {f_name}, alias {u_name} created")
                     print ('\n')
 
             elif short_code == 'dc':
@@ -67,7 +97,7 @@ def main():
                             print('\n')
 
                             for user in display_users():
-                                    print(f"{user.myfirstname}")
+                                    print(f"{u_name}")
 
                             print('\n')
                     else:
@@ -75,7 +105,15 @@ def main():
                             print("You dont seem to have any users saved yet")
                             print('\n')
 
-            # elif short_code == 'del':
+            elif short_code == 'del':
+                print("Enter username so those credentials can be deleted")
+                search_username=input()
+                if check_existing_credentials(search_username):
+                    search_credential=find_credential(search_username)
+                    delete_credential(search_credential)
+                    print(f"{u_name} gone")
+                else:
+                    print("That username is inexistent")
 
 
 
